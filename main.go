d42cb859929e837e80cbdba4d0bf0facf214ad6d
Jason Harris <sirmanlypowers@gmail.com>
2017-10-08 11:54:06 -0700

    Added a generic LUT.

--- main.go
+++ main.go
@@ -10,30 +10,15 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/deadsy/xsynth/pulsego"
-	"math"
 )
 
 //-----------------------------------------------------------------------------
 
-const TABLE_SIZE = (2 << 16)
 const SAMPLE_RATE = 22500
 
-var cos_table = make([]float32, TABLE_SIZE)
-
-func init_tables() {
-	for i, _ := range cos_table {
-		cos_table[i] = float32(math.Cos(float64(i) * 2.0 * math.Pi / float64(TABLE_SIZE)))
-	}
-}
-
-func freq_to_step(f float64) int {
-	return int(math.Ceil(f * float64(TABLE_SIZE) / float64(SAMPLE_RATE)))
-}
-
-func step_to_freq(step int) float64 {
-	return float64(step) * float64(SAMPLE_RATE) / float64(TABLE_SIZE)
-}
+//-----------------------------------------------------------------------------
 
 func sine_wave(pa *pulsego.PulseMainLoop) {
 
@@ -53,16 +38,18 @@ func sine_wave(pa *pulsego.PulseMainLoop) {
 	st.ConnectToSink()
 
 	samples := make([]float32, 64)
+	amp := float32(0.1)
+	t0 := NewLUT_Sine(261.63, SAMPLE_RATE)
+	t1 := NewLUT_Sine(329.63, SAMPLE_RATE)
+	t2 := NewLUT_Sine(392.00, SAMPLE_RATE)
 
-	amp := float32(0.8)
-	posn := 0
-	step := freq_to_step(440.0)
-	fmt.Printf("actual freq is %f\n", step_to_freq(step))
+	//t := NewLUT_Sawtooth(440.0, SAMPLE_RATE)
+	//t := NewLUT_Square(440.0, SAMPLE_RATE)
 
 	for {
 		for i, _ := range samples {
-			samples[i] = cos_table[posn] * amp
-			posn = (posn + step) % TABLE_SIZE
+			y := t0.Sample() + t1.Sample()*t2.Sample()
+			samples[i] = y * amp
 		}
 		st.Write(samples, pulsego.SEEK_RELATIVE)
 	}
@@ -72,7 +59,6 @@ func sine_wave(pa *pulsego.PulseMainLoop) {
 
 func main() {
 
-	init_tables()
 	midi_init()
 
 	pa := pulsego.NewPulseMainLoop()
